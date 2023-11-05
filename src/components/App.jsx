63854ec5649be4c859425d742ec09d458016fc7e
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-11-05 12:45:49 +0400

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,16 @@ import Filter from './Filter/Filter';
 import css from './App.module.css';
 
 export const App = () => {
+  
+  const initialState = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
   const [contacts, setContacts] = useState(() => {
-      const savedContacts = localStorage.getItem('contacts');
-      if (savedContacts) {
-        return JSON.parse(savedContacts);
-      } else {
-        return [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ];
-      }
-    
+    return JSON.parse(localStorage.getItem('contacts')) || initialState;
   });
   
   const [filter, setFilter] = useState('');
